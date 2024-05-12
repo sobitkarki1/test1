661e2080ef96fddc155b87c408b919d3fa54aa7e
Sobit Karki <sobitkarki7@gmail.com>
2024-05-12 20:49:18 +0545

    added $base64Image

--- image-retrieve-test.php
+++ image-retrieve-test.php
@@ -22,6 +22,7 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $imageData = $row['image'];
         $imageType = "image/jpeg"; // mime_content_type($imageData);
+        $base64Image = base64_encode($imageData); // Convert image data to base64
 
          // Display the image
          echo '<img src="data:' . $imageType . ';base64,' . $base64Image . '" alt="Image" width="25%"> <br><br>';
