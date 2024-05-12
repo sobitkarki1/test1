f62656ceb2477d56dd6eb4ed8e690ad8f06dc1c4
Sobit Karki <sobitkarki7@gmail.com>
2024-05-12 19:56:20 +0545

    changing save to submit and changing button attributes

--- add_item.php
+++ add_item.php
@@ -10,7 +10,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . mysqli_connect_error());
 }
- if(isset($_POST['save']))
+ if(isset($_POST['submit']))
  {
     $name = $_POST['name'];
     $description = $_POST['description'];
