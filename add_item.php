731ab0940a483c4f652c4a7dc89d04010a344bc9
Sobit Karki <sobitkarki7@gmail.com>
2024-05-12 20:02:19 +0545

    fixed a naming problem for min_price

--- add_item.php
+++ add_item.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
     $name = $_POST['name'];
     $description = $_POST['description'];
     $image = $_POST['image'];
-    $minimum_price = $_POST['minimum_price'];
+    $minimum_price = $_POST['min_price'];
 
     $sql_query = "INSERT INTO item_details (name,description,image,minimum_price) VALUES ('$name','$description','$image','$minimum_price')"; 
 
