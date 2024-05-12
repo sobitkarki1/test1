774fc5d8665cdf8fb7c8a247280c353d1582c2d7
Sobit Karki <sobitkarki7@gmail.com>
2024-05-12 19:12:45 +0545

    added three files 'as is'

---
+++ add_item.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "itemdb";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+ if(isset($_POST['save']))
+ {
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $image = $_POST['image'];
+    $minimum_price = $_POST['minimum_price'];
+
+    $sql_query = "INSERT INTO item_details (name,description,image,minimum_price) VALUES ('$name','$description','$image','$minimum_price')"; 
+
+    if (mysqli_query($conn,$sql_query))
+    {
+        echo"New Details Entry inserted successfully !";
+    }
+    else{
+    echo "Error: " . $sql . "" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+ }
+ ?>
