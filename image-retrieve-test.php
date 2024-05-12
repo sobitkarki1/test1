25e154b090034581c7b531457e22a2bd1f7659bf
Sobit Karki <sobitkarki7@gmail.com>
2024-05-12 20:44:30 +0545

    for image testing

---
+++ image-retrieve-test.php
@@ -0,0 +1,35 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "itemdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query to retrieve images from the database
+$sql = "SELECT * FROM item_details";
+$result = $conn->query($sql);
+
+// Display retrieved images
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $imageData = $row['image'];
+        $imageType = mime_content_type($imageData);
+
+         // Display the image
+         echo '<img src="data:' . $imageType . ';base64,' . $base64Image . '" alt="Image" width="25%"> <br><br>';
+    }
+} else {
+    echo "No images found.";
+}
+
+// Close connection
+$conn->close();
+?>
